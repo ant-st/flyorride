2d2f81ffba8ad6b6462be1ddf7cc259bcf2ff556
ant-st <antsarnow@gmail.com>
2023-11-07 12:47:54 +0100

    Added: Adding roadtrip to favorites...

--- pages/favorites.vue
+++ pages/favorites.vue
@@ -13,6 +13,15 @@ onMounted(() => {
     <div class="relative px-4 py-10 bg-white sm:mx-8 shadow rounded-3xl sm:p-10 flex flex-col justify-between">
       <h2 class="w-full text-center">Zapisane:</h2>
       <div class="flex flex-line flex-wrap">
+        <CarTravelThumb
+            v-if="favStore.roadtrips"
+            v-for="(roadtrip, index) in favStore.roadtrips"
+            :request = "roadtrip"
+            :index = "index"
+            :travellers = "roadtrip.travellers"
+        />
+      </div>
+      <div class="flex flex-line flex-wrap">
         <FlightThumb
             v-if="favStore.favorites"
             v-for="flight in favStore.favorites"
